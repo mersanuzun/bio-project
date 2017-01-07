4480b4135f9967f8844885096d3479adc040fb18
mersanuzun <mersanuzun@gmail.com>
2017-01-07 17:27:14 +0300

    common iprs and frequencies were added, almost finished

--- script.pl
+++ script.pl
@@ -61,8 +61,9 @@ my @common_IPRs = ();
 
 foreach my $organism (@organisms) {
   $url = "http://www.uniprot.org/uniprot/?sort=score&desc=&compress=no&query=taxonomy:$organism\"$search_term\"&fil=&format=txt&force=yes";  
-  print "$organism is fetching from uniprot.\n";
   my $response = $ua->get($url);
+
+  print "$organism is fetching from uniprot.\n";
   if ($response->is_success) {
     $content = $response->decoded_content;
     my @proteins = split(/\/\/\s/, $content);
@@ -89,46 +90,98 @@ foreach my $organism (@organisms) {
   }else{
     print "$organism was fetched.\n";
   }
-  #else{
-  #  print_organisims(%{$fetched_organisim{$organism}});
-  #}  
 }
 
-sub print_organisims{
-  (my %organisim_proteins) = @_;
-  my $protein_length = scalar(keys %organisim_proteins);
-  print "$protein_length proteins found.\n";
-  foreach my $protein (keys %organisim_proteins){
-    print "$protein \n";
-    foreach my $IPR (keys %{$organisim_proteins{$protein}}){
-      print "$IPR  => " , $organisim_proteins{$protein}{$IPR} , "\n";
-    }
-  }
-  print "********************\n";
-}
-
-
-sub print_all{
-  (my %organisms) = @_;
-  foreach my $organism (keys %organisims) {
-    print "organisim ", $organism, "\n";
-    foreach my $protein (keys %{$organisims{$organism}}){
-      print "$protein \n";
-      foreach my $IPR (keys %{$organisims{$organism}{$protein}}){
-        print "$IPR  => " , $organisims{$organism}{$protein}{$IPR} , "\n";
+foreach my $organism (keys %IPR_frequency){
+  foreach $ipr (keys %{$IPR_frequency{$organism}}){
+    if (isCommon($ipr) == 1){
+      if (isPushed($ipr) == 0){
+        push @common_IPRs, $ipr;
       }
     }
-    print "********************\n";
   }
+}
+
+sub print_all{
+  my $file_name = "all.txt";
+  open(my $out, '>', $file_name)
+  or die "Could not open file ";
+  foreach my $organism (keys %fetched_organisim) {
+    print $out "Organisim\t", $organism, "\n";
+    foreach my $protein (keys %{$fetched_organisim{$organism}}){
+      print $out "\n";
+      print $out "$protein \n";
+      foreach my $IPR (keys %{$fetched_organisim{$organism}{$protein}}){
+        print $out "$IPR\t" , $fetched_organisim{$organism}{$protein}{$IPR} , "\n";
+      }
+    }
+    print $out "\n";
+  }
+  print "All organism informations were printed to $file_name.\n";
 }
 
 sub print_frequencies{
-  foreach my $organism (keys %IPR_frequency){
-    print "Organism: $organism", "\n";
-    foreach $ipr (keys %{$IPR_frequency{$organism}}){
-      print $ipr, ",", $IPR_frequency{$organism}{$ipr}{"name"}, ",", $IPR_frequency{$organism}{$ipr}{"count"}, "\n" ;
+  ($isSort) = @_;
+  my $file_name = "ipr_frequencies.txt";
+  open(my $out, '>', $file_name)
+    or die "Could not open file ";
+  foreach $organism (keys %IPR_frequency){
+    print $out "Organism\t", $organism, "\n" ;
+    print $out "Frequency\tID\tName\n";
+    if ($isSort eq "true"){
+      @organisim_ipr = sort {$IPR_frequency{$organism}{$b}{count} <=> $IPR_frequency{$organism}{$a}{count}} keys %{$IPR_frequency{$organism}};
+    }else{
+      @organisim_ipr = keys %{$IPR_frequency{$organism}};
     }
+    foreach $ipr (@organisim_ipr){   
+      print $out $IPR_frequency{$organism}{$ipr}{"count"}, "\t", $ipr, "\t", $IPR_frequency{$organism}{$ipr}{"name"}, "\n" ;
+    }
+    print $out "\n";
+  }
+  print "IPR frequencies were printed to $file_name.\n";
+}
+
+sub print_common_iprs {
+  my $file_name = "common_iprs.txt";
+  open(my $out, '>', $file_name)
+    or die "Could not open file ";
+  print $out "Organisms\t";
+  print $out "$_\t" foreach (keys %IPR_frequency), "\n";
+  print $out "\n";
+  foreach $ipr (@common_IPRs){
+    print $out $ipr, "\n" ;
+  }
+  print "Common INTERPROs were printed to $file_name.\n";
+}
+
+sub isPushed {
+  (my $ipr) = @_;
+  foreach (@common_IPRs){
+    #print $_->{"id"}, "\n";
+    if ($ipr eq $_){
+      return 1;
+    }
+  }
+  return 0;
+}
+
+sub isCommon {
+  (my $ipr) = @_;
+  my $counter = 0;
+  foreach $organism (keys %IPR_frequency){
+    if(exists $IPR_frequency{$organism}{$ipr}){
+      $counter++;
+    }else{
+      return 0;
+    }
+  }
+  if ($counter == scalar keys %IPR_frequency){
+    return 1;
+  }else{
+    return 0;
   }
 }
 
-print_frequencies();
+print_all();
+print_frequencies("true");
+print_common_iprs();
